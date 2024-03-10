22e0b203932ecf4be75ceb734a5d14d9bb9b3a1a
Your Name <joaohenrique2@hotmail.com>
2024-03-09 22:30:23 -0300

    Operadores logicos

--- Fundamentos/03- Operadores lógicos.js
+++ Fundamentos/03- Operadores lógicos.js
@@ -6,7 +6,7 @@ console.log(!(10>10));
 // || ( OU )
 
 console.log ((10!=10) || (10>10))
-console.log( (10<8) || (10>8))
+console.log( (10<10) || (10>8))
 
 // && E
 console.log((10 == 10 ) && (10>20))
