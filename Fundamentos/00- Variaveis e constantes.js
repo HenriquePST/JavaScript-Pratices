81fa79a39fc19d7b6b0b29eede370bde77d22f0f
Your Name <joaohenrique2@hotmail.com>
2024-03-09 22:27:54 -0300

    Variaveis e constantes

---
+++ Fundamentos/00- Variaveis e constantes.js
@@ -0,0 +1,20 @@
+//const let var
+
+
+/* let permite declaração de variáveis com escopo de bloco, const declara constantes imutáveis,
+ enquanto var tem escopo de função e é menos previsível, sendo preferível usar let e const para
+  evitar problemas de escopo */
+
+var idade = 26;
+console.log (idade);
+idade = 20;
+
+var nome; 
+console.log(nome);
+
+let nomeVariavel = "teste";
+console.log(nomeVariavel);
+
+const nomeConstante = 100;
+console.log(nomeConstante);
+
