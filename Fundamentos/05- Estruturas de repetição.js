863fa1230951dd84924636b2168996221b607946
Your Name <joaohenrique2@hotmail.com>
2024-03-11 15:30:45 -0300

    Repetição  while/do while

--- Fundamentos/05- Estruturas de repetição.js
+++ Fundamentos/05- Estruturas de repetição.js
@@ -3,3 +3,19 @@ for (var i = 0;i<10;i++){
     console.log("teste")
 }
 
+var j = 0 
+//while ( enquanto for verdadeiro faça)
+while (j<10 ){
+    console.log("Ola mundo");
+    j++;
+}
+
+var k = 0;
+// do while faça enquanto
+
+do{
+    console.log("olá mundo");
+    k++;
+} while (k < 10);
+
+
